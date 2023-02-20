2278580ee0ae645d13c8ef32fc228660801d2351
NemethAngela <a.nemet75h@yahoo.com>
2023-02-20 13:13:12 +0100

    MainConsole.java megírása

--- src/MainConsole.java
+++ src/MainConsole.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class MainConsole {
-    
+    public MainConsole(){
+        author();
+        about();
+        task();
+    }
+    public void author(){
+        System.out.println("Sulan Ádám");
+        System.out.println("SZOFT-I-1-N");
+        System.out.println("2023-02-20");
+    }
+    public void about(){
+        System.out.println("Feladat 0358 megoldása:");
+        System.out.println("A paralelepipedon területe:");
+    }
+    private String input(String message){
+        System.out.print(message);
+        Scanner scanner=new Scanner(System.in);
+        String value=scanner.nextLine();
+        return value;
+    }
+    public void task(){
+        String aStr=input("A oldal: ");
+        double a=Double.parseDouble(aStr);
+        String bStr=input("B oldal: ");
+        double b=Double.parseDouble(bStr);
+        String cStr=input("C oldal: ");
+        double c=Double.parseDouble(cStr);
+
+        Paralelepipedon paralelepipedon=new Paralelepipedon();
+        double eredmeny=paralelepipedon.terfogat(a, b, c);
+        System.out.println("A paralelepipedon térfogata: "+eredmeny);
+    }
 }
